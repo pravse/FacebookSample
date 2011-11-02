41eaceb3bf4b575d27766d887f4429544f6a34ed
Praveen Seshadri <praveen_seshadri@hotmail.com>
2011-11-01 22:14:50 -0700

    starting to clean up auth

--- SampleWebRole/Scripts/FBInit.js
+++ SampleWebRole/Scripts/FBInit.js
@@ -28,7 +28,7 @@ function AuthStatusDelegate(response) {
                 // alert("Got the user : " + response.name);
                 FBUserName = response.name;
                 $("#fb-root").trigger("authsuccess", { userId: FBUserId, userName: FBUserName, accessToken: FBAccessToken, isAuthenticated: FBIsAuthenticated } );
-                // alert("Got to auth success");
+                alert("Got to auth success");
             });
         }
         else {
@@ -36,14 +36,14 @@ function AuthStatusDelegate(response) {
             FBUserId = "";
             FBAccessToken = "";
             $("#fb-root").trigger("authfailure", { userId: FBUserId, userName: FBUserName, accessToken: FBAccessToken, isAuthenticated: FBIsAuthenticated });
-            // alert("Got to auth failure");
+            alert("Got to auth failure");
         }
     };
 
 
 // this function will be invoked right after the asynchronous initialization of the FB object
 function PostFBInit() {
-    // alert("Got to PostFBInit");
+    alert("Got to PostFBInit");
     FB.getLoginStatus(AuthStatusDelegate);
 
     //subscribe for any further status changes
